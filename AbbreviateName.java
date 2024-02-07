a6f9fedaaf064b71a38d093f2f11add5229adc58
Vinz Rulloda <102574744+DaVinzci@users.noreply.github.com>
2024-02-07 15:09:57 +0800

    Added Code

--- AbbreviateName.java
+++ AbbreviateName.java
@@ -1,1 +1,10 @@
-// add code here
+public class AbbreviateTwoWords {
+
+    public static String abbrevName(String name) {
+      name = name.toUpperCase();
+      
+      String[] names = name.split(" ");
+      
+      return names[0].charAt(0)+"."+names[1].charAt(0);
+    }
+  }
